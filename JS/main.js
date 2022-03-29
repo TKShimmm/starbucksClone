b5a00b89c2e8e1737316dc6093a7acd0256fb6df
TKShimmm <dev.tkshim@gmail.com>
2022-03-30 02:04:27 +0900

    feat: promotion section

--- JS/main.js
+++ JS/main.js
@@ -26,34 +26,50 @@ window.addEventListener(
       // gsap.to(요소, 지속시간, 옵션);
       gsap.to(badgeEl, 0.6, {
         opacity: 0,
-        display: 'none'
+        display: "none",
       });
     } else {
       //뱃지 보이기
       // gsap.to(요소, 지속시간, 옵션);
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
-        display: 'block'
+        display: "block",
       });
     }
   }, 300)
 );
 // _.throttle(함수, 시간)
 
-
-
-const fadeEls = document.querySelectorAll('.visual .fade-in');
+const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   // gsap.to(요소, 지속시간, 옵션);
   gsap.to(fadeEl, 1, {
-    delay: (index + 1) * .7,
-    opacity: 1
+    delay: (index + 1) * 0.7,
+    opacity: 1,
   });
 });
 
 // new Swiper(선택자, 옵션)
-new Swiper('.notice-line .swiper-container', {
-  direction: 'vertical',
-  autoplay: true,
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  // autoplay: true,
   loop: true,
+});
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자임
+    clickable: true, // 사용자의 페이지 번호 요서 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
 });
