e06ca624aa54419e13252090a45a4c9c7a8eb333
TKShimmm <dev.tkshim@gmail.com>
2022-03-30 15:17:54 +0900

    feat: scrollToTop btn

--- JS/main.js
+++ JS/main.js
@@ -17,9 +17,9 @@ searchInputEl.addEventListener("blur", function () {
 
 const badgeEl = document.querySelector("header .badges");
 
-window.addEventListener(
-  "scroll",
-  _.throttle(function () {
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener("scroll", _.throttle(function () {
     console.log(window.scrollY);
     if (window.scrollY > 500) {
       //뱃지 숨기기
@@ -28,6 +28,10 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      // 버튼 보이기!
+      gsap.to(toTopEl, .2, {
+        x: 0,
+      });
     } else {
       //뱃지 보이기
       // gsap.to(요소, 지속시간, 옵션);
@@ -35,10 +39,22 @@ window.addEventListener(
         opacity: 1,
         display: "block",
       });
+      // 버튼 숨기기!
+      gsap.to(toTopEl, .2, {
+        x: 100,
+      });
     }
   }, 300)
 );
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0,
+  });
+})
+
+
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
